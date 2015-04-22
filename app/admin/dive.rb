83c8f6b17e29a281eb353bb0af4c488d96e11686
Prelang Builder <builder@prelang.com>
2015-04-22 23:14:49 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Dive' to allow for create, update

--- app/admin/dive.rb
+++ app/admin/dive.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Dive do
+  permit_params :title, :date
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
